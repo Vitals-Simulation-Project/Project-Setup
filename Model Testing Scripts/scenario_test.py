410d269263dc688cee4bcbbd76999902ca720024
David Figueras <david.f.sosa33@gmail.com>
2024-11-20 20:13:36 -0500

    Created pull model function

--- Model Testing Scripts/scenario_test.py
+++ Model Testing Scripts/scenario_test.py
@@ -76,15 +76,46 @@ reply = response_json["message"]["content"]
 
 
 
-
 print("Question: ", context_history[-1]["content"])
 print("Answer: ", reply)
 print("Time taken: ", time_taken)
 print("\n\n")
 
 
-# write all the data to a file
-with open("scenario_test_results.txt", "w") as f:
-    f.write("Question: " + context_history[-1]["content"] + "\n")
-    f.write("Answer: " + reply + "\n")
-    f.write("Time taken: " + str(time_taken) + "\n\n\n")
+# wait 3 seconds
+time.sleep(3)
+
+
+context_history = [
+    {
+        "role": "user",
+        "content": "Are you Mario?"
+    },
+]
+
+
+data = {
+    "model": MODEL,
+    "messages": context_history,
+    "stream": False
+}
+
+
+start_time = time.time()
+
+response = requests.post(URL, json=data)
+response_json = json.loads(response.text)
+
+end_time = time.time()
+
+time_taken = end_time - start_time
+
+
+reply = response_json["message"]["content"]
+
+
+
+print("Question: ", context_history[-1]["content"])
+print("Answer: ", reply)
+print("Time taken: ", time_taken)
+print("\n\n")
